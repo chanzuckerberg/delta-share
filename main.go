10edec80040db2f1fa85193b256bd00a7072f134
Allison Doami <adoami@chanzuckerberg.com>
2025-01-22 12:53:33 -0800

    separate

--- main.go
+++ main.go
@@ -27,40 +27,14 @@ type RecipientsResponse struct {
 
 var (
 	// Set using argus set secrets
-	databricksPAT    = os.Getenv("DATABRICKS_PAT")
-	cognitoClientID  = os.Getenv("COGNITO_CLIENT_ID")
-	cognitoIssuerURL = os.Getenv("COGNITO_ISSUER_URL")
+	databricksPAT = os.Getenv("DATABRICKS_PAT")
 )
 
 func main() {
-	if databricksPAT == "" {
-		log.Panic("DATABRICKS_PAT cannot be blank")
-	}
-
-	if cognitoClientID == "" {
-		log.Panic("COGNITO_CLIENT_ID cannot be blank")
-	}
-
-	if cognitoIssuerURL == "" {
-		log.Panic("COGNITO_ISSUER_URL cannot be blank")
-	}
-
-	h := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
-	slog.SetDefault(slog.New(h))
-
-	app := fiber.New()
-
-	app.Use(func(c *fiber.Ctx) error {
-		if c.Path() != "/" && c.Path() != "/health" {
-			logRequest(c)
-		}
-		return c.Next()
-	})
-
-	app.Get("/", healthHandler)
-	app.Get("/health", healthHandler)
-
 	scopes := []string{"openid", "profile", "email"}
+
+	cognitoClientID := "4p1qrneiifjc4npqgoikblc1kv"
+	cognitoIssuerURL := "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_kQfwBKR2t"
 
 	// Apply scopes using SetScopeOptions
 	setScopesOption := client.SetScopeOptions(scopes)
@@ -86,15 +60,33 @@ func main() {
 	var payload map[string]interface{}
 	json.Unmarshal(payloadBytes, &payload)
 	email := payload["email"].(string)
-	username := strings.Split(email, "@")[0]
+	recipientName := strings.Split(email, "@")[0]
+
+	// The following needs to be run in the backend:
+
+	if databricksPAT == "" {
+		log.Panic("DATABRICKS_PAT cannot be blank")
+	}
+
+	h := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
+	slog.SetDefault(slog.New(h))
+
+	app := fiber.New()
+
+	app.Use(func(c *fiber.Ctx) error {
+		if c.Path() != "/" && c.Path() != "/health" {
+			logRequest(c)
+		}
+		return c.Next()
+	})
+
+	app.Get("/", healthHandler)
+	app.Get("/health", healthHandler)
 
 	log.Fatal(app.Listen(":8080"))
 
 	// Replace with your Databricks workspace URL and token
 	databricksURL := "https://czi-shared-infra-czi-sci-general-prod-databricks.cloud.databricks.com"
-
-	// The recipient name you want to check
-	recipientName := username
 
 	// Make the API request
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/2.1/unity-catalog/recipients", databricksURL), nil)
